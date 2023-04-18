7e3cf546b5f91a08f0b18d76b44ba0998c393622
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-04-18 10:58:33 +0100

    product resource

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // WOMEN
+        for ($i = 1; $i <= 12; $i++) {
+            Product::create([
+                'name' => 'Women ' . $i,
+                'slug' => 'women-' . $i,
+                'details' => 'women\'s hoodie',
+                'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
+                'product_code' => '-00',
+                'price' => rand(999, 9999),
+                'quantity' => rand(1, 10),
+            ]);
+        }
+        // MEN
+        for ($i = 1; $i <= 12; $i++) {
+            Product::create([
+                'name' => 'Men ' . $i,
+                'slug' => 'men-' . $i,
+                'details' => 'men\'s hoodie',
+                'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
+                'product_code' => '-00',
+                'price' => rand(999, 9999),
+                'quantity' => rand(1, 10),
+            ]);
+        }
+        // KIDS
+        for ($i = 1; $i <= 12; $i++) {
+            Product::create([
+                'name' => 'Kids ' . $i,
+                'slug' => 'kids-' . $i,
+                'details' => 'kid\'s hoodie',
+                'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
+                'product_code' => '-00',
+                'price' => rand(999, 9999),
+                'quantity' => rand(1, 10),
+            ]);
+        }
+        // HOME GOODS
+        for ($i = 1; $i <= 12; $i++) {
+            Product::create([
+                'name' => 'Home Goods ' . $i,
+                'slug' => 'homegoods-' . $i,
+                'details' => 'homegoods',
+                'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
+                'product_code' => '-00',
+                'price' => rand(999, 9999),
+                'quantity' => rand(1, 10),
+            ]);
+        }
+    }
+}
