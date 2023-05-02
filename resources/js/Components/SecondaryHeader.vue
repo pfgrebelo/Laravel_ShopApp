c2ba7ff5828b5128a3734138829d8b3efba2ee4e
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-05-02 18:17:53 +0100

    added secondary header

---
+++ resources/js/Components/SecondaryHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import Icon from './Icons.vue';
+
+
+</script>
+
+<template>
+   <header class="text-gray-700 bg-gray-300 shadow">
+            <div class="flex justify-between items-center max-w-7x1 mx-auto py-2 px-4 sm:px-6 lg:px-8">
+                <div class="flex items-center leading-tight">
+                    <Link :href="route('welcome')" class="text-gray-700 transition hover:text-yellow-500">Home</Link>
+                    <slot name="breadcrumbs"></slot>
+                </div>
+                <div class="w-1/2">
+                    search
+                </div>
+            </div>
+        </header>
+</template>
--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -2,6 +2,7 @@
 import { Link } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Icon from '@/Components/Icons.vue';
+import SecondaryHeader from '@/Components/SecondaryHeader.vue';
 
 defineProps({
     products: Object,
@@ -12,46 +13,42 @@ defineProps({
 
 <template>
     <AppLayout :title="`Shop ${categoryName}`">
-        <header class="text-gray-700 bg-gray-300 shadow">
-            <div class="flex justify-between items-center max-w-7x1 mx-auto py-2 px-4 sm:px-6 lg:px-8">
-                <div class="flex items-center leading-tight">
-                    <Link :href="route('welcome')" class="text-gray-700 transition hover:text-yellow-500">Home</Link>
-                    <Icon name="angle-right" class="w-4 h-4 fill-current"></Icon>
-                    <span>Shop {{ categoryName }}</span>
-                </div>
-                <div class="w-1/2">
-                    search
-                </div>
-            </div>
-        </header>
+        <SecondaryHeader>
+            <template #breadcrumbs>
+                <Icon name="angle-right" class="w-4 h-4 fill-current"></Icon>
+                <span>Shop {{ categoryName }}</span>
+            </template>
+        </SecondaryHeader>
         <div class="flex">
             <div class="border-r w-1/5">
                 <div class="text-white text-center bg-gray-700 py-4">
                     <p>Shop By Category</p>
                 </div>
                 <div class="flex flex-col divide-y">
-                    <Link v-for="(category, index) in categories" :key="index" :href="route('shop.index', { category: category.slug })"
-                        class="text-left px-4 py-4 transition hover:text-white hover:bg-gray-700 sm:px-6 lg:px-8" 
-                        :class="route().current('shop.index', {category: category.slug}) ? 'bg-gray-700 text-white' : ''">
+                    <Link v-for="(category, index) in categories" :key="index"
+                        :href="route('shop.index', { category: category.slug })"
+                        class="text-left px-4 py-4 transition hover:text-white hover:bg-gray-700 sm:px-6 lg:px-8"
+                        :class="route().current('shop.index', { category: category.slug }) ? 'bg-gray-700 text-white' : ''">
                     {{ category.name }}
                     </Link>
                 </div>
             </div>
             <div class="border-1 w-4/5">
                 <div class="container flex flex-wrap mx-auto">
-                    <Link v-for="(product,index) in products" :key="index" href="#" class="flex flex-col w-full p-4 rounded sm:w-1/2 md:w-1/3">
-                        <img :src="'/storage/images/' + product.image" :alt="product.name" class="h-72 object-cover md:w-72 lg:w-96">
-                        <div class="flex justify-around bg-gray-700 py-2">
-                            <span class="text-yellow-500">
-                                {{ product.price }}
-                            </span>
-                            <span class="text-white">
-                                {{ product.name }}
-                            </span>
-                        </div>
+                    <Link v-for="(product, index) in products" :key="index" href="#"
+                        class="flex flex-col w-full p-4 rounded sm:w-1/2 md:w-1/3">
+                    <img :src="'/storage/images/' + product.image" :alt="product.name"
+                        class="h-72 object-cover md:w-72 lg:w-96">
+                    <div class="flex justify-around bg-gray-700 py-2">
+                        <span class="text-yellow-500">
+                            {{ product.price }}
+                        </span>
+                        <span class="text-white">
+                            {{ product.name }}
+                        </span>
+                    </div>
                     </Link>
                 </div>
             </div>
         </div>
-    </AppLayout>
-</template>
+</AppLayout></template>
